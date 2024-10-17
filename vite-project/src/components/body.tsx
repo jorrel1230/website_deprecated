2b61a36d067f1c5bec07bd82c7cc4522c6c1ecdc
jorrel1230 <jr8867@princeton.edu>
2024-10-17 11:48:24 -0400

    Minor Change for PFP image scaling.

--- vite-project/src/components/body.tsx
+++ vite-project/src/components/body.tsx
@@ -5,34 +5,34 @@ import Footer from "./footer.tsx";
 const Body = () => {
     return (
         <div className="bg-theme-white w-full h-full pl-[8vw] pr-[8vw] pt-[8vw]">
-
                 <SectionCard id="landing_card" title="">
-                        <div className="relative h-[70vh]">
-                                <div
-                                    className="absolute transform -translate-x-1/2 -translate-y-1/2 top-[45%] left-1/3">
-                                        <p className="font-playfair-display text-xl text-theme-green">
-                                                Hi, I'm
-                                        </p>
-                                        <h1 className="font-playfair-display text-8xl text-theme-darkgreen">
-                                                Jorrel Rajan.
-                                        </h1>
-                                        <p className="font-playfair-display text-3xl text-theme-green mt-10">
-                                                Full Stack Developer. <br/> Software Engineer.
-                                        </p>
-                                        <p className="font-playfair-display text-2xl text-theme-darkgreen mt-10">
-                                                Scroll to learn more about me!
-                                        </p>
-                                </div>
+                        <div className="relative h-[70vh] flex items-center justify-center space-x-20">
+                            <div className="">
+                                    <p className="font-playfair-display text-xl text-theme-green">
+                                            Hi, I'm
+                                    </p>
+                                    <h1 className="font-playfair-display text-8xl text-theme-darkgreen">
+                                            Jorrel Rajan.
+                                    </h1>
+                                    <p className="font-playfair-display text-3xl text-theme-green mt-10">
+                                            Full Stack Developer. <br/> Software Engineer.
+                                    </p>
+                                    <p className="font-playfair-display text-2xl text-theme-darkgreen mt-10">
+                                            Scroll to learn more about me!
+                                    </p>
+                            </div>
+                            <div className="">
                                 <img src={profilePhotoURL} alt="Profile Photo"
-                                     className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-[76%] rounded-full"/>
+                                     className="rounded-full"/>
+                            </div>
                         </div>
                 </SectionCard>
 
-                <SectionCard id="about" title="About Me">
-                        .
-                </SectionCard>
+            <SectionCard id="about" title="About Me">
+                .
+            </SectionCard>
 
-                <SectionCard id="experience" title="My Experience">
+            <SectionCard id="experience" title="My Experience">
                         .
                 </SectionCard>
 
