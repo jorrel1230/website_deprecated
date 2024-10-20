b422fe00f4970222f39b853108d79edc57bbc853
Jorrel Rajan <jr8867@princeton.edu>
2024-10-20 13:06:59 -0400

    About me text updates

--- vite-project/src/sub-components/aboutcard.tsx
+++ vite-project/src/sub-components/aboutcard.tsx
@@ -1,16 +1,26 @@
 import SectionCard from "./sectioncard.tsx";
-import placeholderURL from "../assets/profile_photo.svg"
 
 const AboutCard = () => {
     return (
         <SectionCard id="about" title="About Me">
-            <div className="relative h-[70vh] flex">
+            <div className="relative h-[70vh] p-48 flex justify-center items-center">
+                <div>
+                    <p className="font-serif text-xl font-extralight">
+                        My name is Jorrel Rajan. My interest in computer science first began
+                        in middle school, with game development. I explored and used tools such
+                        as <a href="https://unity.com/" className="text-theme-green underline">Unity</a>,
+                        creating basic games.
+                    </p>
 
-                <p>My name is Jorrel Rajan.</p>
+                    <p className="font-serif text-xl font-extralight">
 
-                <img alt="picture of me" src={placeholderURL}
-                     className="rounded-2xl w-2/12 "/>
+                    </p>
 
+                    <p className="font-serif text-xl font-extralight">
+                        I'm currently an undergraduate student working towards my
+                        Bachelors of Science and Engineering at Princeton University.
+                    </p>
+                </div>
             </div>
         </SectionCard>
     )
